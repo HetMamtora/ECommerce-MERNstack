92c9de192db623959c39ea8f3ccea40fb5ab297e
HetMamtora <104263376+HetMamtora@users.noreply.github.com>
2023-05-26 17:01:58 +0530

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,20 +1,4 @@
-/*const mongoose = require("mongoose")
 
-DB_URI="mongodb://0.0.0.0:27017/ECommerce"
-"proxy": "http://192.168.135.214:4000"
-DB_URI="mongodb+srv://Beans:beans27@e-commerce.pq5en9h.mongodb.net/ECommerce?retryWrites=true&w=majority"
-
-const connectDatabase = ()=>{
-
-    mongoose.connect(process.env.DB_URI,{useNewUrlParser:true,useUnifiedTopology:true,useCreateIndex:true}).then(()=>{
-        console.log(`MongoDB Connected with server:${data.connection.host}`)
-    }).catch((err)=>{
-        console.log(err)
-    })
-}
-
-module.exports = connectDatabase
-*/
 const mongoose = require("mongoose");
 
 const connectDatabase = () => {
